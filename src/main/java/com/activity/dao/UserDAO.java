9df9b4019f7b73144ead386ad5db7b2579debb17
kodh1000 <kodh10@gmail.com>
2022-08-25 12:38:53 +0900

    signup update2 commit

--- src/main/java/com/activity/dao/UserDAO.java
+++ src/main/java/com/activity/dao/UserDAO.java
@@ -4,9 +4,13 @@ import java.util.List;
 
 import com.activity.domain.UserDTO;
 
-public interface UserDAO {
-
+public interface UserDAO {	
+	
 	//유저 정보 리스트 
 	public List<UserDTO> getUserList() throws Exception;
+	
+	//회원가입 정보 insert
+	public void insertUser(UserDTO userdto) throws Exception;
+	
 	
 }
--- src/main/java/com/activity/service/UserService.java
+++ src/main/java/com/activity/service/UserService.java
@@ -8,6 +8,8 @@ public interface UserService {
 	//유저 정보 리스트 
 	public List<UserDTO> getUserList() throws Exception;
 	
+	//회원가입 정보 insert
+	public void insertUser(UserDTO userdto) throws Exception;
 }
  
 
